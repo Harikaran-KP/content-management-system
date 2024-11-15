e930c3c95b39c82b4878aa8a329d1313184c929c
Harikaran Kilimangalam Parameswaran <kpharikaran@gmail.com>
2024-11-16 02:59:02 +0530

    Update MainContent.js

--- content-management-system-app/src/pages/mainContent/MainContent.js
+++ content-management-system-app/src/pages/mainContent/MainContent.js
@@ -1,21 +1,14 @@
 import React, { useState, useContext } from 'react';
 import ContentList from '../../components/contentList/ContentList';
-import Drafts from '../../components/drafts/Drafts';
-import Modal from '../../components/modal/Modal';
 import './MainContent.css';
 import { ContentContext } from '../../services/ContentContext';
 
 const MainContentPage = ({ view }) => {
     // Section where all the data related to contents are rendered.
     const { contentList } = useContext(ContentContext);
-    const [showModal, setShowModal] = useState(false);
+    const [, setShowModal] = useState(false);
 
     const handleDelete = () => setShowModal(true);
-
-    const handleConfirmDelete = () => {
-        alert("Item deleted");
-        setShowModal(false);
-    };
 
     return (
         <div className="main-content-page">
