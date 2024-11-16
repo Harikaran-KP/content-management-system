3784f1643731b7e139ad6d474e154aa09f97c5a8
Harikaran Kilimangalam Parameswaran <kpharikaran@gmail.com>
2024-11-16 20:43:16 +0530

    Update TopNavbar.js

--- content-management-system-app/src/components/topNavbar/TopNavbar.js
+++ content-management-system-app/src/components/topNavbar/TopNavbar.js
@@ -5,7 +5,6 @@ const TopNavbar = ({ onToggleSidebar }) => {
   return (
     <div className="top-navbar">
       {/* <button className="hamburger-icon" onClick={onToggleSidebar}>☰</button> */}
-      {/* <div className='hamburger-icon'><HamburgerIcon /></div> */}
       <h2 className="app-title">Media Content Management</h2>
     </div>
   );
